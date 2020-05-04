0fb1ae38ec49b17a0ad61bdd551108071025b109
Vilhelm Prytz <vilhelm@prytznet.se>
2020-05-04 10:24:30 +0200

    tests: initial structure

---
+++ tests/test_react.py
@@ -0,0 +1,14 @@
+####################################################################
+#                                                                  #
+# react-flask                                                      #
+# Copyright (C) 2020, Vilhelm Prytz, <vilhelm@prytznet.se>, et al. #
+#                                                                  #
+# Licensed under the terms of the MIT license, see LICENSE.        #
+# https://github.com/VilhelmPrytz/react-flask                      #
+#                                                                  #
+####################################################################
+
+from react_flask import React
+
+def setup():
+    react = React()
